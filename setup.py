6b99f37bc7982b3c4ee5eb3e698ae94ed9a4b863
redachhaibi <chhaibi.reda@gmail.com>
2022-08-22 21:43:37 +0200

    :hammer: Layout of repo

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import Extension, setup, find_packages
+from os import path
+
+local_path = path.abspath(path.dirname(__file__))
+
+print("Local path: ", local_path)
+print("")
+
+print("Launching setup...")
+# Setup
+setup(
+    name='computational-OT',
+
+    version='0.01',
+
+    description='Computational aspects of optimal transport',
+    long_description=""" Computational solutions to Free Deconvolution.
+    In this module, we explore and benchmark various computational methods 
+    for computing (regularized) optimal transport.
+    """,
+    url='',
+
+    author='Anonymous',
+    author_email='Anonymous',
+
+    license='MIT License',
+
+    install_requires=["numpy", "matplotlib"],
+
+    keywords='',
+
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
+
+    ext_modules=[],
+)
