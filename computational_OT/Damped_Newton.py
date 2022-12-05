e62c36b19806374ccdd13663cfea0cea49d26b81
bosea949 <bosea949@gmail.com>
2022-12-05 16:58:47 +0100

    :hammer: Damped Newton
    Former-commit-id: 5b3cb63b25c59b171637175c4cb2faacfcb58a99

--- computational_OT/Damped_Newton.py
+++ computational_OT/Damped_Newton.py
@@ -11,10 +11,6 @@ class DampedNewton:
         self.rho=rho
         #self.rho_inc=rho_inc
         self.c1=c1
-<<<<<<< HEAD:computational_OT/Damped_Newton.py
-=======
-        #self.z=z
->>>>>>> 41d2897c8cc36f9486e506dc6b3f04f81a7a83eb:computational_OT/linesearchNewton.py
         # DANGEROUS: Duplication of data
         #self.f=f
         #self.g=g
@@ -52,15 +48,6 @@ class DampedNewton:
               reduction_count += 1
             else:
               break
-<<<<<<< HEAD:computational_OT/Damped_Newton.py
-=======
-          
-          # ERROR: No incrementing for Damped Newton
-          # condition_inc = self._objectivefunction(self.x+(self.rho_inc*alpha)*p)>=self._objectivefunction(self.x)+self.c1*(self.rho_inc*alpha)*slope
-          # if reduction_count==0 and condition_inc:
-          #   alpha=self.rho_inc*alpha
-
->>>>>>> 41d2897c8cc36f9486e506dc6b3f04f81a7a83eb:computational_OT/linesearchNewton.py
           return alpha
 
       
