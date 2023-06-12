4fff1389bff6f53d05f36c4db146616a0b3b47dd
bosea949 <bosea949@gmail.com>
2023-06-12 13:46:40 +0200

    :hammer: Minor changes

--- computational_OT/logdomain_sinkhorn.py
+++ computational_OT/logdomain_sinkhorn.py
@@ -15,20 +15,17 @@ class Log_domainSinkhorn:
         return -self.epsilon*np.log( np.sum(self.b[None,:] * np.exp(-H/self.epsilon),1) )  
     
     def mina(self,H):
-        return self.mina_u(H-np.min(H,0)) + np.min(H,0);
+        return self.mina_u(H-np.min(H,0)) + np.min(H,0)
     def minb(self,H):
         return self.minb_u(H-np.min(H,1)[:,None]) + np.min(H,1)
     
-    def update(self, tol = 1e-14, niter = 500):   
+    def update(self, tol = 1e-14, niter = 500):     
         f,g = self.a, self.b
         for i in range(niter):
             g = self.mina(self.C-f[:,None])
             f = self.minb(self.C-g[None,:]) 
             # generate the coupling
-            start = time.time()
             P = self.a[:,None]*np.exp((f[:,None]+g[None,:]-self.C)/self.epsilon)*self.b[None,:] # line (*)
-            end = time.time()
-            print("P time: ", 1e-3*(end-start))
             # check conservation of mass
             self.error.append( np.linalg.norm(np.sum(P,0)-self.b,1) )
             if self.error[i] < tol:
