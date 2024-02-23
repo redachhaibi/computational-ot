:hammer:  Fixed memory leakage problem for pytorch, made changes,   made changes to the  notebook and changed LIN.norm to np.linalg.norm in Sinkhorn

computational_OT/sinkhorn.py
@@ -1,6 +1,4 @@
 import numpy as np
-from numpy import linalg as Lin
-
 
 class Sinkhorn:
 
@@ -33,18 +31,20 @@ class Sinkhorn:
       self.u = self.a / np.dot( self.K, self.v )
       # error computation 1
       r = self.v*np.dot( self.K.T, self.u)
-      self.err_b.append(Lin.norm(r - self.b))
+      self.err_b.append(np.linalg.norm(r - self.b))
       
       # sinkhorn step 2
       self.v = self.b / np.dot( self.K.T, self.u )
       
       # error computation 2
       s = self.u*np.dot( self.K, self.v )
-      self.err_a.append(Lin.norm(s - self.a))
-
+      self.err_a.append(np.linalg.norm(s - self.a))
       iter_condition = (self.err_a[-1]>tol or self.err_b[-1]>tol)
       if iter_condition and i<maxiter :
           i += 1
+      elif np.isnan(self.err_a[-1]) or np.isnan(self.err_b[-1]):
+        print("Sinkhorn is unstable for epsilon: ", self.epsilon, " as there occurs divison by exponentially small values while performing the alternative projection. ")
+        break
       else:
         print("Terminating after iteration: ",i)
         break   

computational_OT/torchlogdomain_sinkhorn.py
@@ -1,40 +1,46 @@
 import torch
 torch.set_default_dtype(torch.float64)
 class torchLog_domainSinkhorn:
-    def __init__(self,a,b,C,epsilon):
+    def __init__(self, a, b, C, epsilon):
         self.a = a
         self.b = b
         self.C = C
         self.epsilon = epsilon
         self.error = []
-    
-    def mina_u(self,H):
-        return -self.epsilon*torch.log(torch.sum(self.a[:,None]*torch.exp(-H/self.epsilon),0))
-    def minb_u(self,H): 
-        return -self.epsilon*torch.log(torch.sum(self.b[None,:]*torch.exp(-H/self.epsilon),1))
-    def mina(self,H):
-        return self.mina_u(H-torch.min(H,0).values)+torch.min(H,0).values
-    def minb(self,H):
-        return self.minb_u(H-torch.min(H,1).values[:,None])+torch.min(H,1).values
-    
-    def update(self,tol=1e-12,niter=500):
-        f,g = self.a.clone(),self.b.clone()
+
+    def mina_u(self, H):
+        return -self.epsilon * torch.log(torch.sum(self.a[:, None] * torch.exp(-H / self.epsilon), 0))
+
+    def minb_u(self, H):
+        return -self.epsilon * torch.log(torch.sum(self.b[None, :] * torch.exp(-H / self.epsilon), 1))
+
+    def mina(self, H):
+        return self.mina_u(H - torch.min(H, 0)[0]) + torch.min(H, 0)[0]
+
+    def minb(self, H):
+        return self.minb_u(H - torch.min(H, 1)[0][:, None]) + torch.min(H, 1)[0]
+
+    def update(self, tol = 1e-12, niter = 500):
+        f, g = self.a, self.b
         for i in range(niter):
-            g = self.mina(self.C-f[:,None])
-            f = self.minb(self.C-g[None,:])
-            #generate the coupling
-            P = self.a[:,None]*torch.exp((f[:,None]+g[None,:]-self.C)/self.epsilon)*self.b[None,:]
-            self.error.append(torch.linalg.norm(torch.sum(P,0)-self.b,1))
-            if self.error[i]<tol:
-                print("Terminating after iteration: ",i)
+            g = self.mina(self.C - f[:, None])
+            f = self.minb(self.C - g[None, :])
+            # generate the coupling
+            P = self.a[:, None] * torch.exp(
+                (f[:, None] + g[None, :] - self.C) / self.epsilon
+            ) * self.b[None, :]
+            # check conservation of mass
+            self.error.append(torch.linalg.norm(torch.sum(P, 0) - self.b, 1).item())
+            if self.error[i] < tol:
+                print("Terminating after iteration: ", i)
                 break
-        
-        #end for
-        if i+1>=niter:
-            print("Terminating after maximal number of iterations: ",niter)
+        # end for
+        if i + 1 >= niter:
+            print("Terminating after maximal number of iterations: ", niter)
         return {
-            'error'       : self.error,
-            'potential_f' : f+self.epsilon*torch.log(self.a).reshape(self.a.shape[0],),
-            'potential_g' : g+self.epsilon*torch.log(self.b).reshape(self.b.shape[0],)
+            "error": self.error,
+            "potential_f": (f + self.epsilon * torch.log(self.a)).reshape(self.a.shape[0],),
+            "potential_g": (g + self.epsilon * torch.log(self.b)).reshape(self.b.shape[0],),
         }
+
 